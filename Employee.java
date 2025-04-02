a36e88a079bd7ffbb1ebe71c1ee1f74dd581e854
Sahil_Thorve_09 <149391162+Sahilthorve09@users.noreply.github.com>
2025-04-02 19:41:18 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,10 @@
+package com.practiceset;
+
+public class Employee {
+	public Employee(int id, String name, double salary) {
+		System.out.println("Empolyee Id: "+id);
+		System.out.println("Employee Name: "+name);
+		System.out.println("Salary: "+salary+" LPA");
+		System.out.println("---------- end -----------");
+	}
+}
---
+++ Student.java
@@ -0,0 +1,11 @@
+package com.practiceset;
+
+public class Student {
+	public Student(String name,int age ) {
+		System.out.println("Name of student: "+ name);
+		System.out.println("Age of student: "+ age);
+		System.out.println("---------- end -----------");
+	}
+	
+	
+}
